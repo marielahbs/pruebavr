e226b0468fbbfb2dfa2a1fd585c61014a07c746b
marielahbs <marielahbarrera@gmail.com>
2018-10-02 13:47:46 -0500

    segundaprueba

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:u/t0UmusllsrgQj4OhVcOYkrzJl6DwVMd7QOC++uaXc='),
 
     'cipher' => 'AES-256-CBC',
 
--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'icopoghru9oezxh8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'ta422fxjyh80dz03'),
+            'username' => env('DB_USERNAME', '  cwy06n9aqmat0hkn'),
+            'password' => env('DB_PASSWORD', 'm1nxh9dlgpdj9u7s '),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
